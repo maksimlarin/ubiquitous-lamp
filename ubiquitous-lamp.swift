7c47f260f43bdcc79c3bddce8c2f24dfdbffc1ff
browning <152813491+maksimlarin@users.noreply.github.com>
2023-12-14 17:21:11 -0300

    Create ubiquitous-lamp.swift

---
+++ ubiquitous-lamp.swift
@@ -0,0 +1,6 @@
+    private func disconnectControlPoints(_ ids: Set<String>,
+                                         in collage: Collage) -> Collage {
+    private func onTwoFingersTranslate(_ gestureState: GestureType.GestureState<CGPoint>,
+                                       in state: AppState) -> AppState {
+        
+        var newState = state
